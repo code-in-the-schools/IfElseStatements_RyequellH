e8bfa1e32a83fc91c7ee901e557d5ecf700d0787
CodingQuell <ryequellhunter@gmail.com>
2020-07-17 17:00:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("My name is...")
+name = "Ryequell"
+print('before for loop')
+for letter in name:
+  print(letter)
+
+print('after for loop')
+
+name = "Ryequell"
+
+print('before for loop')
+
+for letter in name:
+  print(letter)
+  if letter == 'e':
+    print('an e')
+    print('vowel!')
+  print('\n')
