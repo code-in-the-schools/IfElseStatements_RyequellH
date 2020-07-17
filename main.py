949a9b1de4f07ae8f5b3ff82dc8be9a41e9751e3
CodingQuell <ryequellhunter@gmail.com>
2020-07-17 17:36:37 +0000

    Vowels and Consonant

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-print("My name is...")
-name = "Ryequell"
-print('before for loop')
-for letter in name:
-  print(letter)
-
-print('after for loop')
-
 name = "Ryequell"
 
 print('before for loop')
@@ -15,4 +7,18 @@ for letter in name:
   if letter == 'e':
     print('an e')
     print('vowel!')
-  print('\n') 
+  print('\n')
+
+birthday_month = str(input("Birthday Month..."))
+lowercase = birthday_month.lower()
+print(lowercase)
+
+Vowel = ("i", "u", "e", "a", "o")
+I = str(input("My name is..."))
+uI = I.lower
+print(uI)
+for i in range(len(I)):
+  if I[i] in Vowel:
+    print(I[i], " : Vowel")
+  else:
+      print(I[i]," : Consonant")
